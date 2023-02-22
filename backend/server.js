81e3c905f446e22c8e2fb6fd89e95fa79214f46c
JohnSalimRobert <121552921+JohnSalimRobert@users.noreply.github.com>
2023-02-22 20:46:39 +0500

    modified:   backend/package-lock.json 	modified:   backend/package.json 	new file:   backend/routes/userRoutes.js 	modified:   backend/server.js 	new file:   backend/utils.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,16 @@
-import express from 'express';
-import data from './data.js';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import seedRouter from './routes/seedRoutes.js';
-import productRouter from './routes/productRoutes.js';
+import express from "express";
+import data from "./data.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log('connected to DB');
+    console.log("connected to DB");
   })
   .catch((err) => {
     console.log(err.message);
@@ -17,8 +18,16 @@ mongoose
 
 const app = express();
 
-app.use('/api/seed', seedRouter);
-app.use('/api/products', productRouter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 
